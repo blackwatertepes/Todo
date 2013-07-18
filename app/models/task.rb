0d753ccd5dc2210e7495ab025e8cd8c57cfd10db
Tyler J. Kuhn <tyler@wargoats.com>
2013-07-17 20:54:49 -0700

    added task styling

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ActiveRecord::Base
   attr_accessible :completed, :name
 
+  has_many :subtasks, class_name: :Task, foreign_key: :parent_id
+  belongs_to :task, foreign_key: :parent_id
+
   def complete!
     self.completed = true
     self
