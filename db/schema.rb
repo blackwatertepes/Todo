e45f58f3a06fe28eefaf541145bc8c401869f9b5
Tyler J. Kuhn <tyler@wargoats.com>
2013-07-17 20:53:44 -0700

    added parent tasks

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130718020525) do
+ActiveRecord::Schema.define(:version => 20130718034309) do
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
     t.boolean  "completed"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "parent_id"
   end
 
 end
