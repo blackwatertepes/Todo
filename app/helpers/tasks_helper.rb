45784151f6f4077d527a21fd38df72d2ddf2302e
Tyler J. Kuhn <tyler@wargoats.com>
2013-07-19 17:48:06 -0700

    added task show view

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,8 @@
 module TasksHelper
+  def crumbs task
+    bread = task.ancestor_ids.map do |ancestor|
+      {name: ancestor.name, href: task_path(ancestor.id)}
+    end
+    bread.push({name: task.name, href: task_path(task)})
+  end
 end
