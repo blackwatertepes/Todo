9fcc55b4638e117dad56019384bfb7c7a06f1f66
Tyler J. Kuhn <tyler@wargoats.com>
2013-07-20 13:03:57 -0700

    tasks now show subtasks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,6 +1,6 @@
 module TasksHelper
   def crumbs task
-    bread = task.ancestor_ids.map do |ancestor|
+    bread = task.ancestors.map do |ancestor|
       {name: ancestor.name, href: task_path(ancestor.id)}
     end
     bread.push({name: task.name, href: task_path(task)})
