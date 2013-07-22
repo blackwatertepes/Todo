7732661c57787622cf4342fd6e08884d6a576a89
Tyler J. Kuhn <tyler@wargoats.com>
2013-07-22 01:56:26 -0700

    comments are now more viewable

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,7 @@
 module CommentsHelper
+  def comment_info comment
+    user = User.find comment.user_id
+    time = time_ago_in_words comment.created_at
+    "Made by #{user.first_name} #{user.last_name} #{time} ago"
+  end
 end
