22f86e40962f19cb55f127bc63cd369bac2885fa
Tyler J. Kuhn <tyler@wargoats.com>
2013-08-02 17:43:15 -0700

    added company form validation

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,12 @@
 class CompaniesController < ApplicationController
   expose(:company)
   expose(:companies)
+
+  def create
+    if company.save
+      redirect_to :back
+    else
+      render :index
+    end
+  end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,8 @@
 class Company < ActiveRecord::Base
   attr_accessible :name
+
+  validates :name, presence: true
+
   has_many :teams
   has_many :projects
   has_many :employees
