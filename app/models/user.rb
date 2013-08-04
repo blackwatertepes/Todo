e13d00f3f67ab6820f037c144c3cf59d90a39041
Tyler J. Kuhn <tyler@wargoats.com>
2013-08-04 11:30:06 -0700

    could not login

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :remember_me
 
   has_many :tasks
   has_many :employees
