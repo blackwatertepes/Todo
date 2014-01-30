0d669fda8456c10ba73e6ee87b7a48adbf03f2db
Tyler Kuhn <tyler@wargoats.com>
2014-01-30 11:40:45 -0800

    unsigned in users should be redirected to a sign in form

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
 describe PagesController do
+	it 'renders the sign in form' do
+		get :index
+		expect(response.status).to eq 302
+	end
+
 	context "signed in" do
 		let(:user) { create(:user) }
-		
+
 		before(:each) do
 			sign_in :user, user
 		end
